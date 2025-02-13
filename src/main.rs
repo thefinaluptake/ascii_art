b56a0851915b3cb7ef5c11472dc7dfdad44db72b
thefinaluptake <thefinaluptake@proton.me>
2025-02-13 12:32:37 -0500

    load data into array

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,18 @@ use image::ImageReader;
 fn main() -> anyhow::Result<()> {
     let image = ImageReader::open("half_hyacine.jpg")?.decode()?;
 
-    println!(
-        "Successfully loaded image!\nImage size: {} x {}",
-        image.width(),
-        image.height()
-    );
+    let width = image.width();
+    let height = image.height();
+
+    println!("Successfully loaded image!\nImage size: {width} x {height}");
+
+    let image = image.into_rgb8();
+
+    for row in image.rows() {
+        for pixel in row {
+            // println!("{pixel:?}");
+        }
+    }
+
     Ok(())
 }
